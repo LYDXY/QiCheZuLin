031d8e13074c85e65566e59114e490b91c944f44
1542319159@qq.com <1542319159@qq.com>
2016-08-09 17:02:04 +0800

    Failed commit: Failed commit: Default

--- app/src/main/java/com/tongcheng/qichezulin/activity/WangDianSearchActivity.java
+++ app/src/main/java/com/tongcheng/qichezulin/activity/WangDianSearchActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.model.LatLng;
@@ -25,6 +27,7 @@ import com.tongcheng.qichezulin.model.BannerShop;
 import com.tongcheng.qichezulin.model.CityQuModel;
 import com.tongcheng.qichezulin.model.JsonBase;
 import com.tongcheng.qichezulin.model.QuModel;
+import com.tongcheng.qichezulin.utils.Utils;
 import com.tongcheng.qichezulin.utils.UtilsJson;
 import com.tongcheng.qichezulin.utils.UtilsMath;
 import com.tongcheng.qichezulin.view.ExpandableListViewForScrollView;
@@ -51,6 +54,7 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
     ListView lv_fu_jin_men_dian;
     Adapter<BannerShop> shopAdapter;
     List<CityQuModel> cityQuModels;
+    List<BannerShop> shops;
     @ViewInject(R.id.elv_city)
     ExpandableListView elv_city;
     ExpandableAdapter<CityQuModel, QuModel> quModelExpandableAdapter;
@@ -58,6 +62,10 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
     @ViewInject(R.id.iv_return)
     ImageView iv_return;
     com.baidu.mapapi.model.LatLng p1;
+    @ViewInject(R.id.tv_qu_name) //区域名字
+            TextView tv_qu_name;
+    @ViewInject(R.id.tv_near_shop)//附近门店
+            TextView tv_near_shop;
     private String latitude; //纬度
     private String lontitude;//经度
     private String defaultQuid;
@@ -86,7 +94,10 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
             case R.id.iv_return:
                 onBackPressed();
                 break;
-
+            case R.id.tv_near_shop:
+                get_near_Wang_dian_List(lontitude, latitude, "1");
+                tv_qu_name.setText("附近门店");
+                break;
         }
     }
 
@@ -108,7 +119,7 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
 
 
     public void setClickListner() {
-
+        tv_near_shop.setOnClickListener(this);
     }
 
     // 获取二级目录数据
@@ -150,6 +161,9 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
                                         elv_city.collapseGroup(helper.getGroupPosition());
                                     } else {
                                         elv_city.expandGroup(helper.getGroupPosition(), true);
+                                        JLog.i(item.CityId);
+                                        get_Wang_dian_List_by_cityID(item.CityId, "", "");
+
                                     }
                                 }
                             });
@@ -163,6 +177,8 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
                                         @Override
                                         public void onClick(View view) {
                                             JLog.i(item.FName);
+                                            JLog.i(item.PID);
+                                            get_Wang_dian_List(item.PID, "", "", item.FName);
                                         }
                                     });
                         }
@@ -199,8 +215,8 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
     }
 
 
-    // 获取网点列表
-    public void get_Wang_dian_List(String county_id, String keyword, String type_id) {
+    // 获取网点 ----根据城市id
+    public void get_Wang_dian_List_by_cityID(String county_id, String keyword, String type_id) {
         ParamGetWangDianList paramGetWangDianList = new ParamGetWangDianList();
         paramGetWangDianList.county_id = county_id;
         paramGetWangDianList.keyword = keyword;
@@ -210,6 +226,22 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
             @Override
             public void onSuccess(String result) {
                 UtilsJson.printJsonData(result);
+
+                try {
+
+
+                 /*   Gson gson = new Gson();
+                    Type type = new TypeToken<JsonBase<ArrayList<BannerShop>>>() {
+                    }.getType();
+                    JsonBase<ArrayList<BannerShop>> base = gson
+                            .fromJson(result, type);
+                    */
+
+                } catch (Exception E) {
+                    E.printStackTrace();
+                }
+
+
             }
 
             @Override
@@ -230,7 +262,60 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
     }
 
 
-    // 获取附近网点
+    // 获取网点 ----根据区名
+    public void get_Wang_dian_List(String county_id, String keyword, String type_id, final String QUNAME) {
+        ParamGetWangDianList paramGetWangDianList = new ParamGetWangDianList();
+        paramGetWangDianList.county_id = county_id;
+        paramGetWangDianList.keyword = keyword;
+        paramGetWangDianList.type_id = type_id;
+        Callback.Cancelable cancelable
+                = x.http().post(paramGetWangDianList, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                UtilsJson.printJsonData(result);
+                Gson gson = new Gson();
+                Type type = new TypeToken<JsonBase<ArrayList<BannerShop>>>() {
+                }.getType();
+                JsonBase<ArrayList<BannerShop>> base = gson
+                        .fromJson(result, type);
+                if (!base.status.toString().trim().equals("0")) {
+                    if (base.data.size() > 0) {
+                        tv_qu_name.setText(QUNAME);
+                        shopAdapter.clear();
+                        shopAdapter.addAll(base.data);
+                        shopAdapter.notifyDataSetChanged();
+                    } else {
+                        tv_qu_name.setText(QUNAME);
+                        shopAdapter.clear();
+                        shopAdapter.notifyDataSetChanged();
+                        Utils.ShowText2(getApplication(), "该地区没有网点");
+                    }
+                } else {
+                    JLog.i(base.info);
+
+                }
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+
+    // 获取附近网点 |根据距离
     public void get_near_Wang_dian_List(String lng, String lat, String type_id) {
         ParamNearWangDian nearWangDian = new ParamNearWangDian();
         JLog.i(lng + "========" + lat);
@@ -249,29 +334,36 @@ public class WangDianSearchActivity extends Activity implements View.OnClickList
                         .fromJson(result, type);
                 if (!base.status.toString().trim().equals("0")) {
                     if (base.data.size() > 0) {
-                        shopAdapter = new Adapter<BannerShop>(getApplication(), R.layout.listview_item0) {
-                            @Override
-                            protected void convert(final AdapterHelper helper, final BannerShop item) {
-                                final int position = helper.getPosition();
-                                helper
-                                        .setText(R.id.tv_shop_name, item.FShopName)
-                                        .setText(R.id.tv_shop_address, item.FAddress);
-                                LatLng p2 = new LatLng(Double.parseDouble(item.FLatitude), Double.parseDouble(item.FLongitude));
-                                int mishu = (int) DistanceUtil.getDistance(p1, p2);
-                                helper.setText(R.id.tv_hao_far, UtilsMath.getjuli(mishu));//设置距离
-                                helper.getView(R.id.rrl_shop).setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        JLog.i("点击了" + item.FShopName);
+                        if (shopAdapter == null) {
+                            shopAdapter = new Adapter<BannerShop>(getApplication(), R.layout.listview_item0) {
+                                @Override
+                                protected void convert(final AdapterHelper helper, final BannerShop item) {
+                                    final int position = helper.getPosition();
+                                    helper
+                                            .setText(R.id.tv_shop_name, item.FShopName)
+                                            .setText(R.id.tv_shop_address, item.FAddress);
+                                    LatLng p2 = new LatLng(Double.parseDouble(item.FLatitude), Double.parseDouble(item.FLongitude));
+                                    int mishu = (int) DistanceUtil.getDistance(p1, p2);
+                                    helper.setText(R.id.tv_hao_far, UtilsMath.getjuli(mishu));//设置距离
+                                    helper.getView(R.id.rrl_shop).setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            JLog.i("点击了" + item.FShopName);
 
-                                    }
-                                });
+                                        }
+                                    });
 
 
-                            }
-                        };
-                        shopAdapter.addAll(base.data);
-                        lv_fu_jin_men_dian.setAdapter(shopAdapter);
+                                }
+                            };
+                            shopAdapter.addAll(base.data);
+                            lv_fu_jin_men_dian.setAdapter(shopAdapter);
+                        } else {
+                            shopAdapter.clear();
+                            shopAdapter.addAll(base.data);
+                            shopAdapter.notifyDataSetChanged();
+                        }
+
                     }
                 } else {
                     JLog.i(base.info);
