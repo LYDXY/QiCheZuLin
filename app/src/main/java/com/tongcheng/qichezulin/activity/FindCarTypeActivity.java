a87486a8278b0c82cf3ea160e494db56d445863d
1542319159@qq.com <1542319159@qq.com>
2016-08-10 16:33:37 +0800

    Failed commit: Failed commit: Default

--- app/src/main/java/com/tongcheng/qichezulin/activity/FindCarTypeActivity.java
+++ app/src/main/java/com/tongcheng/qichezulin/activity/FindCarTypeActivity.java
@@ -1,11 +1,19 @@
 package com.tongcheng.qichezulin.activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 
 import com.tongcheng.qichezulin.R;
+import com.tongcheng.qichezulin.pulltorefresh.PullToRefreshLayout;
+import com.tongcheng.qichezulin.pulltorefresh.PullableListView;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 /**
  * Created by 林尧 on 2016/8/10.
@@ -13,6 +21,13 @@ import org.xutils.view.annotation.ContentView;
 
 @ContentView(R.layout.activity_find_car_type)
 public class FindCarTypeActivity extends PuTongActivity2 {
+
+
+    @ViewInject(R.id.prl_prl) //下拉刷新控件
+            PullToRefreshLayout prl_prl;
+    @ViewInject(R.id.plv_car_list)
+    PullableListView plv_car_list; //listview
+    private boolean isFirstIn = true;
 
     @Override
     void initData() {
@@ -23,6 +38,17 @@ public class FindCarTypeActivity extends PuTongActivity2 {
     void initView() {
         tv_first.setVisibility(View.VISIBLE);
         tv_first.setText("选择车型");
+        try {
+            prl_prl.setGifRefreshView(new GifDrawable(getResources(), R.mipmap.anim));
+            prl_prl.setGifLoadmoreView(new GifDrawable(getResources(), R.mipmap.anim));
+
+        } catch (Resources.NotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -41,5 +67,31 @@ public class FindCarTypeActivity extends PuTongActivity2 {
         super.onCreate(savedInstanceState);
         initView();
         setListenerOnView();
+        setOnPullListenerOnprl_prl();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // 第一次进入自动刷新
+        if (isFirstIn) {
+            prl_prl.autoRefresh();
+            isFirstIn = false;
+        }
+    }
+
+    public void setOnPullListenerOnprl_prl() {
+        prl_prl.setOnPullListener(new PullToRefreshLayout.OnPullListener() {
+            @Override
+            public void onRefresh(PullToRefreshLayout pullToRefreshLayout) {
+                pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
+            }
+
+            @Override
+            public void onLoadMore(PullToRefreshLayout pullToRefreshLayout) {
+                pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
+            }
+        });
+    }
+
 }
