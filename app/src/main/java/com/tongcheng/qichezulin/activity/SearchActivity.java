0408548b3262f2a8a47f74d2c146361ea8ff30a5
1542319159@qq.com <1542319159@qq.com>
2016-08-17 18:02:41 +0800

    完善搜索界面

--- app/src/main/java/com/tongcheng/qichezulin/activity/SearchActivity.java
+++ app/src/main/java/com/tongcheng/qichezulin/activity/SearchActivity.java
@@ -3,10 +3,12 @@ package com.tongcheng.qichezulin.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.tongcheng.qichezulin.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 /**
@@ -14,14 +16,27 @@ import org.xutils.x;
  */
 @ContentView(R.layout.activity_search)
 public class SearchActivity extends Activity implements View.OnClickListener {
+
+
+    @ViewInject(R.id.tv_back)
+    TextView tv_back;
+
     @Override
     public void onClick(View view) {
 
+        switch (view.getId()) {
+            case R.id.tv_back:
+                onBackPressed();
+                break;
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
+        tv_back.setOnClickListener(this);
     }
+
+
 }
