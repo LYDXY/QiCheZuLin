c4f78b760eb5bd2099031830eb4a7ce77960b785
1542319159@qq.com <1542319159@qq.com>
2016-08-18 17:57:20 +0800

    修改我的订单

--- app/src/main/java/com/tongcheng/qichezulin/activity/MyOrderActivity.java
+++ app/src/main/java/com/tongcheng/qichezulin/activity/MyOrderActivity.java
@@ -1,22 +1,39 @@
 package com.tongcheng.qichezulin.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.View;
 
+import com.flyco.tablayout.SegmentTabLayout;
 import com.jiongbull.jlog.JLog;
 import com.tongcheng.qichezulin.R;
+import com.tongcheng.qichezulin.fragment.FaPiaoTaiTouFragment;
+import com.tongcheng.qichezulin.fragment.PeiSongAddressFragment;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
 
 /**
  * Created by 林尧 on 2016/8/18.
  */
 
 @ContentView(R.layout.activity_my_order)
-public class MyOrderActivity extends PuTongActivity {
+public class MyOrderActivity extends PuTongFragmentActivity {
+
+
+    @ViewInject(R.id.tl_4)
+    SegmentTabLayout tl_4;
+    ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+    private String[] mTitles_2 = {"预约中", "租赁中", "已完成", "已取消"};
+
     @Override
     void initData() {
-
+        fragments.add(FaPiaoTaiTouFragment.getInstance());
+        fragments.add(PeiSongAddressFragment.getInstance());
+        fragments.add(FaPiaoTaiTouFragment.getInstance());
+        fragments.add(PeiSongAddressFragment.getInstance());
     }
 
     @Override
@@ -25,6 +42,7 @@ public class MyOrderActivity extends PuTongActivity {
         tv_first.setText("我的订单");
         tv_second.setText("编辑");
         tv_second.setVisibility(View.VISIBLE);
+        tl_4.setTabData(mTitles_2, this, R.id.fl_change, fragments);
     }
 
 
@@ -46,5 +64,6 @@ public class MyOrderActivity extends PuTongActivity {
         initData();
         initView();
         setListenerOnView();
+
     }
 }
