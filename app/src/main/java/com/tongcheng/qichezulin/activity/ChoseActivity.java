8d7e6090a7502bbe899aedba285d93ecc9f0cad8
1542319159@qq.com <1542319159@qq.com>
2016-08-30 18:01:31 +0800

    完善筛选界面一小部分

--- app/src/main/java/com/tongcheng/qichezulin/activity/ChoseActivity.java
+++ app/src/main/java/com/tongcheng/qichezulin/activity/ChoseActivity.java
@@ -3,25 +3,42 @@ package com.tongcheng.qichezulin.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.jiongbull.jlog.JLog;
+import com.tongcheng.qichezulin.Param.ParamGetCarType;
 import com.tongcheng.qichezulin.R;
+import com.tongcheng.qichezulin.model.CarTypeModel;
+import com.tongcheng.qichezulin.model.JsonBase;
+import com.tongcheng.qichezulin.utils.UtilsJson;
 import com.tongcheng.qichezulin.utils.UtilsString;
 import com.tongcheng.qichezulin.utils.UtilsTiaoZhuang;
 import com.tongcheng.qichezulin.utils.UtilsUser;
 import com.zhy.android.percent.support.PercentRelativeLayout;
 
+import org.xutils.common.Callback;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by 林尧 on 2016/8/3.
  */
 @ContentView(R.layout.activity_chose)
 public class ChoseActivity extends PuTongActivity2 {
+
+
+    @ViewInject(R.id.radioGroup)
+    RadioGroup radioGroup;
 
 
     @ViewInject(R.id.lv_hao_do_you_chose)
@@ -52,7 +69,12 @@ public class ChoseActivity extends PuTongActivity2 {
         tl_4.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
-                JLog.w(position+"");
+                JLog.w(position + "");
+                if (position == 0) {
+                    radioGroup.setVisibility(View.VISIBLE);
+                } else if (position == 1) {
+                    radioGroup.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -60,6 +82,36 @@ public class ChoseActivity extends PuTongActivity2 {
 
             }
         });
+        //单选
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                //获取变更后的选中项的ID
+                int radioButtonId = radioGroup.getCheckedRadioButtonId();
+                //根据ID获取RadioButton的实例
+                RadioButton rb = (RadioButton) ChoseActivity.this.findViewById(radioButtonId);
+                switch (rb.getId()) {
+                    case R.id.radio_first:
+                        JLog.w("价格不限");
+                        break;
+                    case R.id.radio_second:
+                        JLog.w("150");
+                        break;
+                    case R.id.radio_three:
+                        JLog.w("300");
+                        break;
+                    case R.id.radio_fourth:
+                        JLog.w("500");
+                        break;
+                    case R.id.radio_fifth:
+                        JLog.w("1500");
+                        break;
+                }
+
+
+            }
+        });
+
     }
 
 
@@ -79,5 +131,45 @@ public class ChoseActivity extends PuTongActivity2 {
         super.onResume();
         JLog.w("onResume");
 
+    }
+
+    //获取车的类型
+    public void get_carType(){
+        ParamGetCarType paramGetCarType = new ParamGetCarType();
+        Callback.Cancelable cancelable
+                = x.http().post(paramGetCarType, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                UtilsJson.printJsonData(result);
+                Gson gson = new Gson();
+                Type type = new TypeToken<JsonBase<ArrayList<CarTypeModel>>>() {
+                }.getType();
+                JsonBase<ArrayList<CarTypeModel>> base = gson
+                        .fromJson(result, type);
+                if (!base.status.toString().trim().equals("0")) {
+                    if (base.data.size() > 0) {
+                    }
+                } else {
+                    JLog.w(base.info);
+
+                }
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 }
